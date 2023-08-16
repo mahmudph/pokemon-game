9d9dfe6a010ceab5a7ae1d288d5eb84491cad77a
mahmud <mahmud120398@gmail.com>
2023-08-16 08:30:02 +0700

    refactor: code clean up

--- app/src/main/java/id/myone/pokemongame/models/DreamWorld.kt
+++ app/src/main/java/id/myone/pokemongame/models/DreamWorld.kt
@@ -7,7 +7,4 @@ import com.google.gson.annotations.SerializedName
 data class DreamWorld(
     @SerializedName("front_default")
     val frontDefault: String,
-
-    @SerializedName("front_female")
-    val frontFemale: Any
 )
