ac6da7722646f2b7dee03e266b9b1e36c9cf33c8
mahmud <mahmud120398@gmail.com>
2023-08-16 08:29:55 +0700

    refactor: update common properties

--- app/src/main/java/id/myone/pokemongame/common/common.kt
+++ app/src/main/java/id/myone/pokemongame/common/common.kt
@@ -5,7 +5,13 @@
 
 package id.myone.pokemongame.common
 
+import android.graphics.Color
+
 
 val pokemonStateTypes = listOf(
     "hp", "attack", "defense", "special-attack", "special-defense", "speed"
+)
+
+val availableColor = listOf(
+    Color.BLUE,Color.MAGENTA, Color.CYAN, Color.GREEN, Color.RED, Color.YELLOW
 )
